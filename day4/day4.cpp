c671c71bac649fac916d365f7074626e5019e5d8
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-12-05 16:32:42 -0500

    okay apparently reverting back to c++98 is the way to go here

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -1,8 +1,6 @@
 #include <filesystem>
 #include <fmt/core.h>
 #include <fstream>
-#include <ranges>
-#include <set>
 #include <vector>
 
 std::vector<std::vector<char>> parse_word_search(std::ifstream& file)
@@ -41,74 +39,64 @@ int main(int argc, char* argv[])
     auto wordsearch = parse_word_search(file);
     file.close();
 
-    size_t total_appearances = 0;
+    const auto width = wordsearch[0].size();
+    const auto height = wordsearch.size();
 
+    const auto count_occurrences_in_window =
+        [&wordsearch, width, height](int64_t x, int64_t y) -> int64_t {
+        int64_t result = 0;
 
-    std::set<std::pair<size_t, size_t>> coords;
+        constexpr auto is_phrase = [](auto begin, auto end) -> bool {
+            constexpr std::string_view phrase = "XMAS";
+            return std::equal(begin, end, phrase.begin(), phrase.end()) ||
+                   std::equal(begin, end, phrase.rbegin(), phrase.rend());
+        };
 
-    // create a sliding 2D window over the wordsearch
-    size_t y = 0;
-    for (auto row = wordsearch.begin(); row + 4 != wordsearch.end(); ++row) {
-        size_t x = 0;
-        for (long i = 0; i + 4 < row->size(); ++i) {
-            using subrange = std::ranges::subrange<decltype(row->begin())>;
+        std::vector<std::string> kernel;
 
-            constexpr auto is_phrase = [](auto begin, auto end) -> bool {
-                constexpr std::string_view phrase = "XMAS";
-                return std::equal(begin, end, phrase.begin(), phrase.end()) ||
-                       std::equal(begin, end, phrase.rbegin(), phrase.rend());
-            };
+        bool hgood = x + 4 <= width;
+        bool vgood = y + 4 <= height;
 
-            // check horizontal
-            for (long j = 0; j < 4; ++j) {
-                std::pair<size_t, size_t> startcoords = {x, y + i};
-                if (!coords.contains(startcoords)) {
-                    auto crow = row + j;
-                    auto subrow_start = crow->begin() + i;
-                    auto subrow_end = crow->begin() + i + 4;
-
-                    if (is_phrase(subrow_start, subrow_end)) {
-                        total_appearances += 1;
-                    }
-                }
-                coords.insert(startcoords);
-            }
-
-            // check vertical
-            for (long j = 0; j < 4; ++j) {
-                std::pair<size_t, size_t> startcoords = {x, y};
-                std::string col;
-                for (size_t k; )
-                    col += row[i];
-
-                if (is_phrase(col.begin(), col.end()))
-                    total_appearances += 1;
-            }
-
-            // check diagonals
-            std::array diagonals = {
-                std::string{
-                    window[0][0],
-                    window[1][1],
-                    window[2][2],
-                    window[3][3],
-                },
-                std::string{
-                    window[3][0],
-                    window[2][1],
-                    window[1][2],
-                    window[0][3],
-                },
-            };
-
-            for (const auto& d : diagonals)
-                if (is_phrase(d.begin(), d.end()))
-                    total_appearances += 1;
-
-            x += 1;
+        if (hgood && vgood) {
+            // diagonals
+            kernel.push_back(std::string{
+                wordsearch[y + 0][x + 0],
+                wordsearch[y + 1][x + 1],
+                wordsearch[y + 2][x + 2],
+                wordsearch[y + 3][x + 3],
+            });
+            kernel.push_back(std::string{
+                wordsearch[y + 3][x + 0],
+                wordsearch[y + 2][x + 1],
+                wordsearch[y + 1][x + 2],
+                wordsearch[y + 0][x + 3],
+            });
         }
-        y += 1;
-    }
+
+        if (hgood)
+            kernel.push_back(std::string{
+                wordsearch[y].begin() + x,
+                wordsearch[y].begin() + x + 4,
+            });
+
+        if (vgood)
+            kernel.push_back(std::string{
+                wordsearch[y + 0][x],
+                wordsearch[y + 1][x],
+                wordsearch[y + 2][x],
+                wordsearch[y + 3][x],
+            });
+
+        for (const auto& d : kernel)
+            result += is_phrase(d.begin(), d.end());
+
+        return result;
+    };
+
+    size_t total_appearances = 0;
+    for (int64_t y = 0; y < wordsearch.size(); ++y)
+        for (int64_t x = 0; x < wordsearch[0].size(); ++x)
+            total_appearances += count_occurrences_in_window(x, y);
 
     fmt::println("times xmas found {}", total_appearances);
 
