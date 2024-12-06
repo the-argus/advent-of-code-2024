9b632a804f9eaa48f48a124b2287b11bb4d0f4e3
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-12-06 02:01:52 -0500

    day6 part two, bugged. fix after i sleep

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -3,13 +3,44 @@
 #include <fmt/core.h>
 #include <fstream>
 #include <ranges>
+#include <unordered_map>
 #include <vector>
 
 enum class TileFlags : uint8_t
 {
-    Air = 0b0000,
-    IsObstacle = 0b0001,
-    IsVisited = 0b0010,
+    // clang-format off
+    Air =           0b0000000,
+    IsObstacle =    0b0000001,
+    IsVisited =     0b0000010,
+    North =         0b0000100,
+    South =         0b0001000,
+    East =          0b0010000,
+    West =          0b0100000,
+    IsGuard =       0b1000000
+    // clang-format on
+};
+
+struct Visit
+{
+    std::pair<int64_t, int64_t> location;
+    uint8_t direction;
+
+    bool operator==(const Visit& other) const
+    {
+        return location.first == other.location.first &&
+               location.second == other.location.second &&
+               direction == other.direction;
+    }
+};
+
+struct VisitHasher
+{
+    std::size_t operator()(Visit const& v) const
+    {
+        return std::hash<int64_t>()(v.location.first &
+                                        (v.location.second << 24) ^
+                                    uint8_t(v.direction));
+    }
 };
 
 int main(int argc, char* argv[])
@@ -58,6 +89,7 @@ int main(int argc, char* argv[])
         assert(guard.first == std::numeric_limits<size_t>::max());
 
         guard = {idx - line.begin(), map.size() - 1};
+        map[guard.second][guard.first] &= uint8_t(TileFlags::IsGuard);
 
         switch (*idx) {
         case '>':
@@ -77,46 +109,130 @@ int main(int argc, char* argv[])
         }
     }
 
-    // trace guards path through the area
-    while (guard.first + guard_direction.first < map[0].size() &&
-           guard.first + guard_direction.first >= 0 &&
-           guard.second + guard_direction.second < map.size() &&
-           guard.second + guard_direction.second >= 0) {
+    using vec = std::pair<int64_t, int64_t>;
 
-        uint8_t next = map[guard.second + guard_direction.second]
-                          [guard.first + guard_direction.first];
+    std::unordered_map<Visit, bool, VisitHasher> visits;
+    std::vector<Visit> visit_sequence;
 
-        if (!(next & uint8_t(TileFlags::IsObstacle))) {
-            uint8_t& current = map[guard.second][guard.first];
-            current |= uint8_t(TileFlags::IsVisited);
+    const auto is_route_looping =
+        [&visits, &visit_sequence](vec _guard, vec _guard_direction,
+                                   const std::vector<Row>& map) -> bool {
+        vec guard = _guard;
+        vec guard_direction = _guard_direction;
 
-            guard.first += guard_direction.first;
-            guard.second += guard_direction.second;
-            continue;
+        while (guard.first + guard_direction.first < map[0].size() &&
+               guard.first + guard_direction.first >= 0 &&
+               guard.second + guard_direction.second < map.size() &&
+               guard.second + guard_direction.second >= 0) {
+
+            uint8_t next = map[guard.second + guard_direction.second]
+                              [guard.first + guard_direction.first];
+
+            Visit visit{.location = guard};
+
+            if (guard_direction == std::pair<int64_t, int64_t>{1, 0}) {
+                visit.direction = uint8_t(TileFlags::East);
+            } else if (guard_direction == std::pair<int64_t, int64_t>{0, 1}) {
+                visit.direction = uint8_t(TileFlags::South);
+            } else if (guard_direction == std::pair<int64_t, int64_t>{-1, 0}) {
+                visit.direction = uint8_t(TileFlags::West);
+            } else if (guard_direction == std::pair<int64_t, int64_t>{0, -1}) {
+                visit.direction = uint8_t(TileFlags::North);
+            } else {
+                fmt::println("wut");
+                std::abort();
+            }
+
+            visit_sequence.push_back(visit);
+
+            if (visits.contains(visit)) {
+                // we have already been here and will continue to loop
+                return true;
+            }
+            visits.insert({visit, true});
+
+            if (!(next & uint8_t(TileFlags::IsObstacle))) {
+                guard.first += guard_direction.first;
+                guard.second += guard_direction.second;
+                continue;
+            }
+
+            if (guard_direction == std::pair<int64_t, int64_t>{1, 0}) {
+                guard_direction = {0, 1};
+            } else if (guard_direction == std::pair<int64_t, int64_t>{0, 1}) {
+                guard_direction = {-1, 0};
+            } else if (guard_direction == std::pair<int64_t, int64_t>{-1, 0}) {
+                guard_direction = {0, -1};
+            } else if (guard_direction == std::pair<int64_t, int64_t>{0, -1}) {
+                guard_direction = {1, 0};
+            } else {
+                fmt::println("failure");
+                std::abort();
+            }
         }
+        return false;
+    };
 
-        if (guard_direction == std::pair<int64_t, int64_t>{1, 0}) {
-            guard_direction = {0, 1};
-        } else if (guard_direction == std::pair<int64_t, int64_t>{0, 1}) {
-            guard_direction = {-1, 0};
-        } else if (guard_direction == std::pair<int64_t, int64_t>{-1, 0}) {
-            guard_direction = {0, -1};
-        } else if (guard_direction == std::pair<int64_t, int64_t>{0, -1}) {
-            guard_direction = {1, 0};
-        } else {
-            fmt::println("failure");
-            std::abort();
+    size_t loops = 0;
+
+    for (size_t y = 0; y < map.size(); ++y) {
+        for (size_t x = 0; x < map[0].size(); ++x) {
+
+            uint8_t& tile = map[y][x];
+
+            if (tile & uint8_t(TileFlags::IsObstacle) ||
+                tile & uint8_t(TileFlags::IsGuard))
+                continue;
+
+            auto path_point =
+                std::find_if(visit_sequence.begin(), visit_sequence.end(),
+                             [x, y](const Visit& visit) -> bool {
+                                 return visit.location.first == x &&
+                                        visit.location.second == y;
+                             });
+
+            // okay, we are placing the tile in a place that gets traversed,
+            // rewind to before that point
+            if (path_point != visit_sequence.end()) {
+                auto invalidated =
+                    std::ranges::subrange(path_point, visit_sequence.end());
+                for (const Visit& v : invalidated)
+                    visits.erase(v);
+                visit_sequence.erase(invalidated.begin(), invalidated.end());
+            }
+
+            uint8_t copy = tile;
+            tile &= uint8_t(TileFlags::IsObstacle);
+            const auto location =
+                visit_sequence.empty() ? guard : visit_sequence.back().location;
+            auto dir = guard_direction;
+            if (!visit_sequence.empty()) {
+                switch (visit_sequence.back().direction) {
+                case uint8_t(TileFlags::North):
+                    dir = {0, -1};
+                    break;
+                case uint8_t(TileFlags::South):
+                    dir = {0, 1};
+                    break;
+                case uint8_t(TileFlags::East):
+                    dir = {1, 0};
+                    break;
+                case uint8_t(TileFlags::West):
+                    dir = {-1, 0};
+                    break;
+                default:
+                    fmt::println("i dont understand why");
+                    std::abort();
+                }
+            }
+            bool looping = is_route_looping(location, dir, map);
+            fmt::println("looping: {}", looping);
+            loops += looping;
+            tile = copy;
         }
     }
-    // mark the last square we walk off as visited
-    map[guard.second][guard.first] |= uint8_t(TileFlags::IsVisited);
 
-    size_t total = 0;
-    for (const auto& row : map)
-        for (uint8_t tile : row)
-            total += (tile & uint8_t(TileFlags::IsVisited)) != 0;
-
-    fmt::println("visited tiles: {}", total);
+    fmt::println("looping paths: {}", loops);
 
     return 0;
 }
