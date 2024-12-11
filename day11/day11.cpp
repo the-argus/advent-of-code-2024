afcbdc15e9929477423cb83d8013eb9aab9fdcc4
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-12-11 13:16:17 -0500

    dynamic programming day11 part 2

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -1,44 +1,51 @@
 #include "aoc.h"
 #include "shorthand.h"
 #include <ranges>
-#include <vector>
+#include <unordered_map>
 
-struct Node
+using pair = std::pair<u64, u64>;
+
+struct pairhash
 {
-    u64 value;
-    std::vector<std::unique_ptr<Node>> children;
-
-    u64 populate_recursive_to_depth(u64 depth);
+    std::size_t operator()(pair const& v) const
+    {
+        return std::hash<u64>()(v.first ^ v.second);
+    }
 };
 
-u64 Node::populate_recursive_to_depth(u64 depth)
+u64 populate_recursive_to_depth(const pair args)
 {
+    static std::unordered_map<pair, u64, pairhash> resultcache;
+    auto [value, depth] = args;
+
     if (depth == 0)
         return 1;
 
-    if (value == 0) {
-        value = 1;
-        return populate_recursive_to_depth(depth - 1);
-    } else if (auto str = std::to_string(value); str.length() % 2 == 0) {
-        auto lefti = str | std::views::take(str.length() / 2);
-        auto righti = str | std::views::drop(str.length() / 2);
-        std::string left(std::begin(lefti), std::end(lefti));
-        std::string right(std::begin(righti), std::end(righti));
+    if (resultcache.contains(args))
+        return resultcache.at(args);
 
-        children.push_back(
-            std::make_unique<Node>(Node{.value = u64(std::stol(left))}));
-        children.push_back(
-            std::make_unique<Node>(Node{.value = u64(std::stol(right))}));
+    if (value == 0) {
+        resultcache[args] = populate_recursive_to_depth({1, depth - 1});
+    } else if (auto str = std::to_string(value); str.length() % 2 == 0) {
+
+        const auto lefti = str | std::views::take(str.length() / 2);
+        const auto righti = str | std::views::drop(str.length() / 2);
+
+        const std::string left(std::begin(lefti), std::end(lefti));
+        const std::string right(std::begin(righti), std::end(righti));
+
+        const pair largs = {std::stol(left), depth - 1};
+        const pair rargs = {std::stol(right), depth - 1};
+
+        const u64 lres = populate_recursive_to_depth(largs);
+        resultcache[args] = lres + populate_recursive_to_depth(rargs);
     } else {
-        value = value * 2024;
-        return populate_recursive_to_depth(depth - 1);
+        resultcache[args] =
+            populate_recursive_to_depth({value * 2024, depth - 1});
     }
 
-    u64 total = 0;
-    for (auto& child : children)
-        total += child->populate_recursive_to_depth(depth - 1);
-    children.clear();
-    return total;
+    u64 result = resultcache.at(args);
+    return result;
 }
 
 int main(int argc, char* argv[])
@@ -51,16 +58,14 @@ int main(int argc, char* argv[])
     std::string line;
     std::getline(file, line);
 
-    constexpr auto strtonode = [](auto numstr) {
-        u64 i = std::stol(std::string(numstr.begin(), numstr.end()));
-        return std::make_unique<Node>(Node{.value = i});
+    constexpr auto strtoint = [](auto numstr) {
+        return std::stol(std::string(numstr.begin(), numstr.end()));
     };
-    auto nodes =
-        line | std::views::split(' ') | std::views::transform(strtonode);
+    auto root = line | std::views::split(' ') | std::views::transform(strtoint);
 
     u64 total = 0;
-    for (const std::unique_ptr<Node>& node : nodes)
-        total += node->populate_recursive_to_depth(75);
+    for (i64 i : root)
+        total += populate_recursive_to_depth({i, 75});
 
     fmt::println("got nodes at max depth: {}", total);
 
